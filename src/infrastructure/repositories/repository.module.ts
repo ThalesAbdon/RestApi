0eff4cccb716e031b57d186e3818ecf660db774e
Thales Sousa <thales.sousa@contavoltz.com>
2023-07-30 15:26:21 -0300

    build: change connection for  mongo atlas

--- src/infrastructure/repositories/repository.module.ts
+++ src/infrastructure/repositories/repository.module.ts
@@ -1,10 +1,11 @@
+import "dotenv/config" 
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserRepository } from './users/user.repository';
 import { User, UserSchema } from '../database/model/user.model';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/admin'),
+  imports: [MongooseModule.forRoot(`${process.env.MONGO_URL}`),
   MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),],
   exports:  [UserRepository],
   providers: [UserRepository],
